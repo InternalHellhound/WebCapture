1dd7df3866ba763b72b83519f2e011759e7ace60
tawhidmonowar <tawhidmonowar@gmail.com>
2025-02-01 22:18:54 +0600

    Update InternetCheck.java

--- app/src/main/java/com/tawhid/webcapture/InternetCheck.java
+++ app/src/main/java/com/tawhid/webcapture/InternetCheck.java
@@ -3,15 +3,12 @@ package com.tawhid.webcapture;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-
 public class InternetCheck {
-
     public static boolean isConnected(WebViewActivity context) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
-
     public static boolean internetDisconnected(WebViewActivity context) {
         // Perform your actions when there's no internet connection
         return false;
